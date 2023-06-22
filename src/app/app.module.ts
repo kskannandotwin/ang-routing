e649b644c79ec7a3a3ca51cd42f36b6b6f899bac
kannanATexcelforte <kannan@excelforte.com>
2023-06-22 16:25:56 +0530

    22.06.2023
    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { HomeComponent } from './home/home.component';
 import { SinglePostComponent } from './single-post/single-post.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,11 +16,7 @@ import { SinglePostComponent } from './single-post/single-post.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'posts', component: PostListComponent },
-      { path: 'post/:id/:title', component: SinglePostComponent }
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
