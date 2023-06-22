033b0dcd9a450ad57e5ad8365610b2b0a9fe421b
kannanATexcelforte <kannan@excelforte.com>
2023-06-22 14:28:58 +0530

    22.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,11 @@ export class AppComponent {
     const obsTest$ = new Observable(observer => {
       observer.next('Returned from observable');
       observer.next('This is the second return from observable');
+
+      setTimeout(() => {
+        observer.next('This is from timeout function');
+      }, 2000);
+
       observer.next('This is the third return from observable');
     }).subscribe(value => {
       console.log(value);
