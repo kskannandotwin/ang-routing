b8b34c1a849e37d57364b9e0a4298695893677f9
kannanATexcelforte <kannan@excelforte.com>
2023-06-22 16:44:22 +0530

    22.06.2023
    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { SinglePostComponent } from './single-post/single-post.component';
+import { FournotfourComponent } from './fournotfour/fournotfour.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
-  { path: 'post/:id/:title', component: SinglePostComponent }
+  { path: 'post/:id/:title', component: SinglePostComponent },
+  { path: '**', component: FournotfourComponent }
 ];
 
 @NgModule({
